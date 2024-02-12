8d8cdf710348bf4864af1a03d3ac24469d3996f0
unknown <emmco96@gmail.com>
2024-02-12 19:53:27 +0100

    new

--- index.js
+++ index.js
@@ -38,3 +38,21 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+  var open = false;
+
+function openNav() {
+    var sideNav = document.getElementById("mySidenav");
+    
+    if (sideNav.style.width === "0px" || sideNav.style.width === "") {
+        sideNav.style.width = "250px";
+        open = true;
+    } else {
+        sideNav.style.width = "0";
+        open = false;
+    }
+}
